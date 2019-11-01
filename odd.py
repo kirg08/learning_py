bb1019ab4292f219a02ffe5db703b93e69c780e5
Kirill <kirill.g08@mail.ru>
2019-11-01 16:07:02 +0400

    add random end time

--- odd.py
+++ odd.py
@@ -1,4 +1,6 @@
 from datetime import  datetime
+import random
+import time
 
 odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
 right_this_minute = datetime.today().minute
@@ -7,4 +9,6 @@ for i in range(5):
         print("This minute seems a little add.")
     else:
         print("Not an add minute.")
+    wail_time = random.randint(1, 60)
+    time.sleep(wail_time)
     
